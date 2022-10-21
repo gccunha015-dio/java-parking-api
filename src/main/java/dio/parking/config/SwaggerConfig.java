a1275bbf6c2682ca2dd8ba134f0187898b027a4c
Gabriel C. Cunha <gccunha015@gmail.com>
2022-10-21 14:49:36 -0300

    Configured OpenAPI server

--- src/main/java/dio/parking/config/SwaggerConfig.java
+++ src/main/java/dio/parking/config/SwaggerConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
   @Bean
   public OpenAPI api() {
     return new OpenAPI()
+        .addServersItem(new Server().url("/"))
         .info(new Info()
             .title("Parking API")
             .description("REST API to manage a parking system.")
